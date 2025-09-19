1e14051715c4a373709d6316e303c24e212511bf
Nik Revenco <pm@nikrev.com>
2025-09-19 01:15:56 +0100

    test: add UI tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,122 +1,133 @@
+#![cfg(test)]
 use docstr::docstr;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+const AGE: u32 = 19;
 
-    const AGE: u32 = 19;
+#[test]
+fn ui() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/*.rs");
+}
 
-    /// Works with constants
-    #[test]
-    fn constant() {
-        const A: &str = docstr!(
-            /// foo
-            /// bar
-        );
+#[test]
+fn empty() {
+    const A: &str = docstr!(
+        ///
+    );
 
-        assert_eq!(A, "foo\nbar", "join with newline");
+    assert_eq!(A, "");
+}
 
-        const B: &str = docstr!(
-            /// foo
-            /// bar
-            ///
-        );
+/// Works with constants
+#[test]
+fn constant() {
+    const A: &str = docstr!(
+        /// foo
+        /// bar
+    );
 
-        assert_eq!(B, "foo\nbar\n", "newline at end");
-    }
+    assert_eq!(A, "foo\nbar", "join with newline");
 
-    /// Can do string interpolation
-    #[test]
-    fn format() {
-        assert_eq!(
-            docstr!(format
+    const B: &str = docstr!(
+        /// foo
+        /// bar
+        ///
+    );
+
+    assert_eq!(B, "foo\nbar\n", "newline at end");
+}
+
+/// Can do string interpolation
+#[test]
+fn format() {
+    assert_eq!(
+        docstr!(format
                 /// Hello, my name is {}
                 /// and I am {AGE} years old
             "Bob"),
-            format!("Hello, my name is Bob\nand I am {AGE} years old")
-        );
-    }
+        format!("Hello, my name is Bob\nand I am {AGE} years old")
+    );
+}
 
-    /// NO macro, but `{}`
-    #[test]
-    fn fake_interpolation() {
-        assert_eq!(
-            docstr!(
-                /// I am {AGE} years old
-            ),
-            "I am {AGE} years old"
-        );
-    }
+/// NO macro, but `{}`
+#[test]
+fn fake_interpolation() {
+    assert_eq!(
+        docstr!(
+            /// I am {AGE} years old
+        ),
+        "I am {AGE} years old"
+    );
+}
 
-    /// String interpolation using a custom macro
-    #[test]
-    fn formatln() {
-        macro_rules! formatln {
+/// String interpolation using a custom macro
+#[test]
+fn formatln() {
+    macro_rules! formatln {
             ($($tt:tt)*) => {
                 format!($($tt)*) + "\n"
             };
         }
 
-        assert_eq!(
-            docstr!(
-                formatln
-                /// Hello, my name is {}
-                /// and I am {AGE} years old
-                "Bob"
-            ),
-            format!("Hello, my name is Bob\nand I am {AGE} years old\n")
-        );
-
-        // same, but comma after `formatln`
-
-        assert_eq!(
-            docstr!(
-                formatln,
-                /// Hello, my name is {}
-                /// and I am {AGE} years old
-                "Bob"
-            ),
-            format!("Hello, my name is Bob\nand I am {AGE} years old\n")
-        );
-    }
-
-    /// Accepts arguments before the string
-    #[test]
-    fn writeln() {
-        use std::fmt::Write as _;
-        let mut s = String::new();
+    assert_eq!(
         docstr!(
-            writeln, s
-            /// hello
-            /// {}
-            "world"
-        )
-        .unwrap();
+            formatln
+            /// Hello, my name is {}
+            /// and I am {AGE} years old
+            "Bob"
+        ),
+        format!("Hello, my name is Bob\nand I am {AGE} years old\n")
+    );
 
-        assert_eq!(s, "hello\nworld\n");
+    // same, but comma after `formatln`
 
-        // Same, but a comma after `s`
-
-        let mut s = String::new();
+    assert_eq!(
         docstr!(
-            writeln, s,
-            /// hello
-            /// {}
-            "world"
-        )
-        .unwrap();
+            formatln,
+            /// Hello, my name is {}
+            /// and I am {AGE} years old
+            "Bob"
+        ),
+        format!("Hello, my name is Bob\nand I am {AGE} years old\n")
+    );
+}
 
-        assert_eq!(s, "hello\nworld\n");
-    }
+/// Accepts arguments before the string
+#[test]
+fn writeln() {
+    use std::fmt::Write as _;
+    let mut s = String::new();
+    docstr!(
+        writeln, s
+        /// hello
+        /// {}
+        "world"
+    )
+    .unwrap();
 
-    #[test]
-    fn escape() {
-        assert_eq!(
-            docstr!(
-                /// hello "world" ' \ ! ()
-                /// ///\\/\// \u{0032}
-            ),
-            "hello \"world\" ' \\ ! ()\n///\\\\/\\// \\u{0032}"
-        );
-    }
+    assert_eq!(s, "hello\nworld\n");
+
+    // Same, but a comma after `s`
+
+    let mut s = String::new();
+    docstr!(
+        writeln, s,
+        /// hello
+        /// {}
+        "world"
+    )
+    .unwrap();
+
+    assert_eq!(s, "hello\nworld\n");
+}
+
+#[test]
+fn escape() {
+    assert_eq!(
+        docstr!(
+            /// hello "world" ' \ ! ()
+            /// ///\\/\// \u{0032}
+        ),
+        "hello \"world\" ' \\ ! ()\n///\\\\/\\// \\u{0032}"
+    );
 }
